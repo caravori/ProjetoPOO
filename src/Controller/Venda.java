f0419a5596659e5af42b5b9bf6dedba5cddaa49c
itootoito <88070196+itootoito@users.noreply.github.com>
2021-07-29 17:57:44 -0300

    finalizei a classe venda
    por favor ver se esta certo!

--- src/Controller/Venda.java
+++ src/Controller/Venda.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Venda {
 	private int idVenda;
 	private double valor;
@@ -10,18 +13,98 @@ public class Venda {
 	private Data data;
 	private Horario horario;
 	
-	public Venda() {
+	//CONSTRUCTOR
+	public Venda(int idVenda, double valor, Vendedor vendedor, Cliente cliente, Moto moto, Carro carro, Data data, Horario horario ) {
+		
+		this.idVenda = idVenda;
+		this.valor = valor;
+		this.vendedor = vendedor;
+		this.cliente = cliente;
+		this.moto = moto;
+		this.carro = carro;
+		this.data = data;
+		this.horario = horario;
 		
 	}
 	
-	public void setId(int id) {
-		this.idVenda = id;
+	//METODO INSERIR
+	public inserirVenda() throwsIOException
+	{
+		FileWriter addVenda = new FileWriter("src/arquivos/vendas.csv", true);
+			addVenda.write(this.getId() + ";");
+			addVenda.write(this.getvalor() + ";");
+			addVenda.write(this.getVendedor() + ";");
+			addVenda.write(this.getCliente() + ";");
+			addVenda.write(this.getMoto() + ";");
+			addVenda.write(this.getCarro() + ";");
+			addVenda.wrtie(this.getData() + ";");
+			addVenda.write(this.getHorario() + ";");
+	}
+	
+	//SETTERS
+	public void setId(int idVenda) {
+		this.idVenda = idVenda;
 	}
 	public void setValor(double valor) {
 		this.valor = valor;
 	}
-	public void setVendedor() {
-		//TODO
+	
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public void setMoto(Moto moto) {
+		this.moto = moto;
+	}
+	
+	public void setCarro(Carro carro) {
+		this.carro = carro;
+	}
+	
+	public void setData(Data data) {
+		this.data = data;
+	}
+	
+	public void setHorario(Horario horario) {
+		this.horario = horario;
+	}
+	
+	
+	//GETTERS
+	public int getId() {
+		return idVenda;
+	}
+	
+	public double getvalor() {
+		return valor;
+	}
+	
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public Moto getMoto() {
+		return moto;
+	}
+	
+	public Carro getCarro() {
+		return carro;
+	}
+	
+	public Data getData() {
+		return data;
+	}
+	
+	public Horario getHorario() {
+		return horario;
 	}
 	
 }
